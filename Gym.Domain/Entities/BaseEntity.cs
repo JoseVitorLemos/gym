cb2e633717486c5c621af5c2d3a14fad4935270c
José Vitor <jose81073175@gmail.com>
2024-03-08 20:16:49 -0300

    refactor: add createAt on BaseEntity

--- Gym.Domain/Entities/BaseEntity.cs
+++ Gym.Domain/Entities/BaseEntity.cs
@@ -4,11 +4,13 @@ public abstract class BaseEntity
 {
     public Guid Id { get; protected set; }
     public bool Status { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
 
     public BaseEntity()
     {
         Id = Guid.NewGuid();
         Status = true;
+        CreatedAt = DateTime.Now;
     }
 
     public void AlterStatus(bool status = false)
